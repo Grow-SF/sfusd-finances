fd2f5f0bb7686dc182d28e401efb5d2f904d5427
strangevc <tara@strangevc.com>
2026-01-31 21:37:42 +0000

    config updates

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,6 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = { output: "export" };
+const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/sfusd-finances",
+};
 export default nextConfig;
